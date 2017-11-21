51ad9cbbc0832e7078954708c27a9295bb497e6a
Sarp Solakoglu <sarp.solakoglu@travelportdigital.com>
2017-11-21 21:54:07 +0000

    - refactoring

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const axios = require('axios');
 const fx = require('money');
 const twit = require('twit');
@@ -5,45 +6,68 @@ const schedule = require('node-schedule');
 const express = require('express');
 const fs = require('fs');
 
-var config = require('./env.json')[process.env.NODE_ENV || 'development'];
-
 const port = process.env.PORT || 3000;
 
 var app = express();
 
-var currentValue;
 const nerdesiniz = 'Gezi zamanı Dolar 1.92 oldu diye vatan haini diyenler şimdi Dolar %s, nerdesiniz?';
 
 var T = new twit({
-    consumer_key:         config.consumer_key,
-    consumer_secret:      config.consumer_secret,
-    access_token:         config.access_token,
-    access_token_secret:  config.access_token_secret,
+    consumer_key:         process.env.T_CONSUMER_KEY,
+    consumer_secret:      process.env.T_CONSUMER_SECRET,
+    access_token:         process.env.T_ACCESS_TOKEN,
+    access_token_secret:  process.env.T_ACCESS_TOKEN_SECRET,
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 });
   
-let tweet = (rate) => {
-    var tw = nerdesiniz.replace('%s', rate.toFixed(4));
-    T.post('statuses/update', { status: tw }, function(err, data, response) {
-        var stringData = JSON.stringify(data);
-        fs.writeFile('server.log', stringData + '\n', (err) => {
-            if (err) {
-                console.log("Couldn't log to file");
-            }
-        });
-    });
+var executeTweet = () => {
+    getText()
+        .then((tweetText) => tweet(tweetText))
+        .then((dataString) => logToFile(dataString))
+        .catch((errorMessage) => logToFile(errorMessage));
 };
 
-schedule.scheduleJob({hour: 08, minute: 00}, () => {
-    axios.get('https://api.fixer.io/latest')
+var getText = () => {
+    return axios.get('https://api.fixer.io/latest')
         .then((response) => response.data)
         .then((data) => fx.rates = data.rates)
         .then(() => fx(1).from("USD").to("TRY"))
-        .then((rate) => tweet(rate));
+        .then((rate) => nerdesiniz.replace('%s', rate.toFixed(4)));
+}
+
+var tweet = (tweetText) => {
+    return new Promise((resolve, reject) => {
+        T.post('statuses/update', { status: tweetText }, function(err, data, response) {
+            if (err) {
+                reject(err.message);
+            } else {
+                resolve(JSON.stringify(data));
+            }
+        });
+    });
+}
+
+var logToFile = (message) => {
+    fs.writeFile('server.log', message + '\n', (err) => {
+        if (err) {
+            console.log("Couldn't log to file");
+        }
+    });
+}
+
+schedule.scheduleJob({hour: 21, minute: 53}, () => {
+    executeTweet();
 });
 
-app.get('/', (req, res) => {
-    res.send('Nerdesiniz.js');
+app.get('/', async (req, res, next) => {
+    try {
+        var nerdesinizText = await getText();
+        res.send({
+            nerdesinizText 
+        });
+    } catch (e) {
+        next(e);
+    }
 });
 
 app.listen(port, () => {
